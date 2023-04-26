82e4b0bf923578cffb1bc7e6c4f792f27cad090b
Kachi-Okorie <51110360+Kachi-Okorie@users.noreply.github.com>
2023-04-26 23:20:36 +1000

    modifiedapp

--- week4_code.js
+++ week4_code.js
@@ -141,6 +141,35 @@ app.get("/division", (req,res)=>{
     }
 });
 
+const fraction= (num1,num2) => {
+  return (num1*num2)/num1;
+}
+app.get("/fraction", (req,res)=>{
+  try{
+  const num1= parseFloat(req.query.num1);
+  const num2=parseFloat(req.query.num2);
+  if(isNaN(num1)) {
+      logger.error("num1 is incorrectly defined");
+      throw new Error("num1 incorrectly defined");
+  }
+  if(isNaN(num2)) {
+      logger.error("num2 is incorrectly defined");
+      throw new Error("num2 incorrectly defined");
+  }
+  
+  if (num1 === NaN || num2 === NaN) {
+      console.log()
+      throw new Error("Parsing Error");
+  }
+  logger.info('Parameters '+num1+' and '+num2+' received for multiplication and division');
+  const result = fraction(num1,num2);
+  res.status(200).json({statuscocde:200, data: result }); 
+  } catch(error) { 
+      console.error(error)
+      res.status(500).json({statuscocde:500, msg: error.toString() })
+    }
+});
+
 const port=3040;
 app.listen(port,()=> {
     console.log("hello i'm listening to port " +port);
